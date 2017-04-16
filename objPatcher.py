8dacc0982238c073558ecbb00cbb325411b17b16
maf-ia <mafteam.ia@gmail.com>
2017-04-16 13:10:23 +0200

    manage args for loading file at start

--- objPatcher.py
+++ objPatcher.py
@@ -7,4 +7,6 @@ import PyQt4.QtGui as QtGui
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     mainwindow = mainWindow.MainWindow()
+    if len(sys.argv) > 1:
+        mainwindow.loadFile( sys.argv[1] )
     sys.exit(app.exec_())
